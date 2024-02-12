d5623ef78005146ce4d3ca8cd1e41cc1925f168f
Santiago <70394887+sacastrot@users.noreply.github.com>
2024-02-12 16:12:20 -0500

    Update mi_primera_app.py
    Add authour and co author

--- mi_primera_app.py
+++ mi_primera_app.py
@@ -3,11 +3,11 @@ import streamlit as st
 
 # Título de la app y autor
 st.title("Mi primera app")
-st.write("Esta app fue elaborada por COLOQUE AQUÍ SU NOMBRE")
+st.write("Esta app fue elaborada por Santiago Castro")
 
 # Preguntar el nombre al usuario
 nombre_usuario = st.text_input("Por favor, introduce tu nombre")
 
 # Imprimir el mensaje de bienvenida
 if nombre_usuario:
-    st.write(f"{nombre_usuario}, te doy la bienvenida a mi primera app.")
+    st.write(f"{nombre_usuario}, te doy la bienvenida a mi primera app. Recibí ayuda de ChatGPT")
